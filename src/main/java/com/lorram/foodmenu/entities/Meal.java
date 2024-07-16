7c40584d15a1716b5dcb5d9790f66cdef941e282
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-07-16 11:35:09 -0300

    Project created

---
+++ src/main/java/com/lorram/foodmenu/entities/Meal.java
@@ -0,0 +1,11 @@
+package com.lorram.foodmenu.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_meal")
+public class Meal {
+	
+	//TODO
+}
