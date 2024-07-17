bc1957c3fea09617e13c8c588032f67035d3342d
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-07-17 11:55:48 -0300

    MealService

--- src/main/java/com/lorram/foodmenu/services/MealService.java
+++ src/main/java/com/lorram/foodmenu/services/MealService.java
@@ -1,6 +1,9 @@
 package com.lorram.foodmenu.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,4 +23,37 @@ public class MealService {
 		return list.map(x -> new MealDTO(x));
 	}
 	
+	public MealDTO findById(Long id) {
+		Optional<Meal> entity = repository.findById(id);
+		Meal meal = entity.orElseThrow(() -> new RuntimeException());
+		return new MealDTO(meal);
+	}
+	
+	public MealDTO insert(MealDTO dto) {
+		Meal meal = new Meal();
+		fromDto(dto, meal);
+		meal = repository.save(meal);
+		return new MealDTO(meal);
+	}
+	
+	public MealDTO update(MealDTO dto, Long id) {
+		Meal meal = repository.getReferenceById(id);
+		fromDto(dto, meal);
+		meal = repository.save(meal);
+		return new MealDTO(meal);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			} 
+		catch (EmptyResultDataAccessException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	private void fromDto(MealDTO mealDto, Meal meal) {
+		meal.setName(mealDto.name());
+		meal.setDescription(mealDto.description());
+	}
 }
