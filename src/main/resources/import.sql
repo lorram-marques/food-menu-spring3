4cc844f331173ff3688a25cdc0d46616a41af4ea
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-07-22 11:26:31 -0300

    Exceptions

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_user (name, email) VALUES ('Alex', 'alex@gmail.com');
+INSERT INTO tb_user (name, email) VALUES ('Maria', 'maria@gmail.com');
+
+INSERT INTO tb_meal (name, description) VALUES ('X-Burger', 'PLACEHOLDER');
+
+INSERT INTO tb_review (title, body, user_id) VALUES ('PH', 'PH', 1);
+
+INSERT INTO tb_review_meal (review_id, meal_id) VALUES (1, 1);
